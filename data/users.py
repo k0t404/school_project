77dd0b52b966c733b4bed9658c76e9a523edc9c0
k0t404 <eggr.5877@gmail.com>
2023-11-05 02:14:36 +0300

    Переделал работу с дб на SQLAlch

---
+++ data/users.py
@@ -0,0 +1,21 @@
+import sqlalchemy
+from flask_login import UserMixin
+from .db_session import SqlAlchemyBase
+from sqlalchemy_serializer import SerializerMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+class User(SqlAlchemyBase, UserMixin, SerializerMixin):
+    __tablename__ = 'users'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    user_id = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    about = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
--- excel_to_sql.py
+++ excel_to_sql.py
@@ -1,84 +1,87 @@
 import os
 import sqlite3
 import openpyxl
+from data import db_session
+from data.lesssons import Lesssons
 
 
-def export_to_sqlite():
-    '''Экспорт данных из xlsx в sqlite'''
+def export_to_sqlite(grade, file_name):
+    # Подключение к бд
+    db_session.global_init("db/logs.db")
+    db_sess = db_session.create_session()
 
-    # 1. Создание и подключение к базе
-    # Получаем текущую папку проекта
-    prj_dir = os.path.abspath(os.path.curdir)
-    a = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    # Имя базы
-    base_name = 'timetable.sqlite3'
-    # метод sqlite3.connect автоматически создаст базу, если ее нет
-    connect = sqlite3.connect(prj_dir + '/' + base_name)
-    # курсор - это специальный объект, который делает запросы и получает результаты запросов
-    cursor = connect.cursor()
-    # создание таблицы если ее не существует
-    cursor.execute('CREATE TABLE IF NOT EXISTS timetables (lesson_pos, lesson, cabinet, class_letter, day)')
-
-    # 2. Работа c xlsx файлом
     # Читаем файл и лист1 книги excel
-    file_to_read = openpyxl.load_workbook('Расписание.xlsx', data_only=True)
-    sheet = file_to_read['5 класс']
-    # Циxкл по строкам начиная со второй (в первой заголовки)
-    classes = [sheet.cell(1, col).value for col in range(1, 21) if sheet.cell(1, col).value]
+    file_to_read = openpyxl.load_workbook(f'{file_name}.xlsx', data_only=True)
+    sheet = file_to_read[f'{int(grade)} класс']
+    # Циxкл по строкам
+    classes = [sheet.cell(1, col).value for col in range(1, sheet.max_column + 1) if sheet.cell(1, col).value]
     count_days = 0
-    count_letters = 0
-    for row in range(2, sheet.max_row + 1):
+    day = None
+    # Цикл по строчкам
+    for row in range(2, 41):
+        count_classes = 0
         count_days += 1
         if count_days == 1:
             day = sheet.cell(row, 1).value
         elif count_days % 9 == 0:
             day = sheet.cell(row, 1).value
             count_days = 1
-        # Объявление списка
+        # Обновление списка
         data = []
         count_columns = 0
-        # Цикл по столбцам от 1 до 4 ( 22 не включая)
-        for col in range(2, 21):
+        # Цикл по столбцам
+        for col in range(2, len(classes) * 3):
             count_columns += 1
             # value содержит значение ячейки с координатами row col
             value = sheet.cell(row, col).value
             # Список который мы потом будем добавлять
             data.append(value)
             if count_columns == 3:
-                data.append(classes[count_letters])
-                count_letters += 1
+                data.append(classes[count_classes])
+                count_classes += 1
                 count_columns = 0
                 data.append(day)
-                cursor.execute("INSERT INTO timetables VALUES (?, ?, ?, ?, ?);",
-                               (data[0], data[1], data[2], data[3], data[4]))
-                data = []
-        count_letters = 0
-    # 3. Запись в базу и закрытие соединения
-        # Вставка данных в поля таблицы
+                # Вставка данных в поля таблицы
+                lessons = Lesssons()
+                lessons.lesson_pos = data[0]
+                lessons.lesson = data[1]
+                lessons.cabinet = data[2]
+                lessons.class_letter = data[3]
+                lessons.day = data[4]
+                db_sess.add(lessons)
 
-    # сохраняем изменения
-    connect.commit()
-    # закрытие соединения
-    connect.close()
+                data = []
+
+    # сохраняем изменения и закрываем соединение
+    db_sess.commit()
 
 
 def clear_base():
-    '''Очистка базы sqlite'''
+    # Очистка базы sqlite
 
     # Получаем текущую папку проекта
     prj_dir = os.path.abspath(os.path.curdir)
 
     # Имя базы
-    base_name = 'timetables.sqlite3'
+    base_name = 'timetable.db'
 
-    connect = sqlite3.connect(prj_dir + '/' + base_name)
+    connect = sqlite3.connect(prj_dir + '/db/' + base_name)
     cursor = connect.cursor()
 
     # Запись в базу, сохранение и закрытие соединения
-    cursor.execute("DELETE FROM timetables")
+    cursor.execute("DELETE FROM timetable")
     connect.commit()
     connect.close()
 
 
+def process_control(*grade_start_end, file_name, wreck_apart=False):
+    if wreck_apart:
+        pass
+    elif grade_start_end:
+        grade_start, grade_end = grade_start_end[0]
+        for grade in range(int(grade_start), int(grade_end) + 1):
+            export_to_sqlite(grade, file_name)
+
+
 # Запуск функции
-export_to_sqlite()
+process_control((5, 11), file_name='Расписание')
