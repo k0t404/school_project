e71931de93ce19ce8d4cb0d62c7cb2c18945a519
k0t404 <eggr.5877@gmail.com>
2023-11-26 21:40:58 +0300

    изменения 2.0

--- bot2.py
+++ bot2.py
@@ -45,26 +45,22 @@ def get_text_messages(message):
         if authorized_user.about == 'завуч':
             qu2(message)
         else:
-            bot.send_message(message.from_user.id, "У вас нету прав для изменения в расписании",
-                             reply_markup=start_keyboard())
+            bot.send_message(message.from_user.id, "У вас нет прав для изменения в расписании",
+                             reply_markup=start_keyboard(message.from_user.id))
 
     elif message.text.split()[0] == 'Изменение':
         if authorized_user.about == 'завуч':
             clas, number, cabinet, lesson = message.text.split()[1:]
             ismeneniya(message, clas, number, cabinet, lesson)
         else:
-            bot.send_message(message.from_user.id, "У вас нету прав для изменения в расписании",
-                             reply_markup=start_keyboard())
+            bot.send_message(message.from_user.id, "У вас нет прав для изменения в расписании",
+                             reply_markup=start_keyboard(message.from_user.id))
 
     # начало всей аторизации
     elif message.text == 'Авторизоваться':
         qu3(message)
         # Ниже финальная версия и ее нужно будет вернуть, н ос ней станет сложнее работать, т.к. мы не
         # сможем заводить новые аккунты в боте
-        '''if not authorized_user:
-            qu3(message)
-        else:
-            bot.send_message(message.from_user.id, "Вы уже авторизованы")'''
 
     # продолжение авторизации (определение пользователя)
     elif message.text.lower() == 'учитель' or message.text.lower() == 'завуч' or message.text.lower() == 'ученик':
@@ -76,11 +72,14 @@ def get_text_messages(message):
 
     # авторизация учеников
     elif message.text.split()[0].lower() == 'авторизация':
-        clas = f'{message.text.split()[1]} "{message.text.split()[2].upper()}" класс'
-        if db_sess.query(Lesssons).filter(Lesssons.class_letter == clas).first():
-            authorization(message)
+        if not authorized_user:
+            clas = f'{message.text.split()[1]} "{message.text.split()[2].upper()}" класс'
+            if db_sess.query(Lesssons).filter(Lesssons.class_letter == clas).first():
+                authorization(message)
+            else:
+                bot.send_message(message.from_user.id, "Такого класса не существует!!!!")
         else:
-            bot.send_message(message.from_user.id, "Такого класса не существует!!!!")
+            bot.send_message(message.from_user.id, "Вы уже авторизованы")
 
     # вывод функций бота, наверное
     elif message.text == 'Что может бот?':
--- comm2.py
+++ comm2.py
@@ -4,13 +4,15 @@ from con2 import BOT_TOKEN
 from telebot import types
 from data import db_session
 from data.lesssons import Lesssons
-from  data.users import User
+from data.users import User
 from data.changes import Changes
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
 
-def start_keyboard():
+def start_keyboard(user_id):
+    '''db_sess = db_session.create_session()
+    authorized_user = db_sess.query(User).filter(User.user_id == user_id)'''
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)  # создание новых кнопок
     btn1 = types.KeyboardButton('Что может бот?')
     btn2 = types.KeyboardButton('Расписание_уроков')
@@ -26,23 +28,24 @@ def starts(message):
     btn1 = types.KeyboardButton("👋 Поздороваться")
     markup.add(btn1)
     bot.send_message(message.from_user.id, "👋 Привет! Я бот Артем, и я помогу тебе в расписании!",
-                     reply_markup=start_keyboard())
+                     reply_markup=start_keyboard(message.from_user.id))
 
 
 def search(message):
-    bot.send_message(message.from_user.id, 'Выберите действие', reply_markup=start_keyboard())
+    bot.send_message(message.from_user.id, 'Выберите действие', reply_markup=start_keyboard(message.from_user.id))
 
 
 def question(message):
     bot.send_message(message.from_user.id, "Все вопросы можно писать на нашу почту")
-    bot.send_message(message.from_user.id, "cot5626@mail.ru", reply_markup=start_keyboard())
+    bot.send_message(message.from_user.id, "cot5626@mail.ru", reply_markup=start_keyboard(message.from_user.id))
 
 
 def helper(message):
     bot.send_message(message.from_user.id, "Я бот Артем.")
     bot.send_message(message.from_user.id, "Я могу:")
     bot.send_message(message.from_user.id, "Вывести расписание на сегодня")
-    bot.send_message(message.from_user.id, "Внести изменения в рассписание класса", reply_markup=start_keyboard())
+    bot.send_message(message.from_user.id, "Внести изменения в рассписание класса",
+                     reply_markup=start_keyboard(message.from_user.id))
 
 
 def raspisanie(clas, message, autharized_student=False):
@@ -54,7 +57,7 @@ def raspisanie(clas, message, autharized_student=False):
 
     if datetime.datetime.today().weekday() > 4:
         bot.send_message(message.from_user.id, 'Бот отказывается работать в выходные. Иди к МЭШ-у',
-                         reply_markup=start_keyboard())
+                         reply_markup=start_keyboard(message.from_user.id))
     else:
         date = days[datetime.datetime.today().weekday()]
         db_sess = db_session.create_session()
@@ -63,17 +66,20 @@ def raspisanie(clas, message, autharized_student=False):
             lesson = [row.lesson_pos, row.lesson, row.cabinet, row.class_letter, row.day]
             lessons.append(lesson)
         if lessons:
-            bot.send_message(message.from_user.id, f'Расписание для {clas}', reply_markup=start_keyboard())
+            bot.send_message(message.from_user.id, f'Расписание для {clas}',
+                             reply_markup=start_keyboard(message.from_user.id))
             for row in lessons:
                 if None not in row:
-                    bot.send_message(message.from_user.id, '       '.join(row), reply_markup=start_keyboard())
+                    bot.send_message(message.from_user.id, '       '.join(row),
+                                     reply_markup=start_keyboard(message.from_user.id))
         else:
             bot.send_message(message.from_user.id, 'Такой класс не был найден, попробуй снова',
-                             reply_markup=start_keyboard())
+                             reply_markup=start_keyboard(message.from_user.id))
 
 
 def qu1(message):
     bot.send_message(message.from_user.id, "Введите: Расписание 'ваш класс(к примеру 1 ь)'")
+
 
 def qu2(message):
     bot.send_message(message.from_user.id, "Введите: Изменение 'класс номер_урока кабинет название_урока")
@@ -109,16 +115,18 @@ def authorization(message):
         print(f'{message.text[0]} "{message.text[1]}" класс')
     db_sess.add(user)
     db_sess.commit()
-    bot.send_message(message.from_user.id, 'готово', reply_markup=start_keyboard())
+    bot.send_message(message.from_user.id, 'готово', reply_markup=start_keyboard(message.from_user.id))
 
 
 def ismeneniya(message, clas, number, cabinet, lesson):
     days = ['ПОНЕДЕЛЬНИК', 'ВТОРНИК', 'СРЕДА', 'ЧЕТВЕРГ', 'ПЯТНИЦА']
     if datetime.datetime.today().weekday() > 4:
         bot.send_message(message.from_user.id, 'В выходные невозможно вносить изменения',
-                         reply_markup=start_keyboard())
+                         reply_markup=start_keyboard(message.from_user.id))
     else:
         date = days[datetime.datetime.today().weekday()]
+        clas = f'{clas[:2]} "{clas[-1]}" класс'
+        print(clas)
         db_sess = db_session.create_session()
         items = Changes()
         items.lesson_pos = number
@@ -126,14 +134,22 @@ def ismeneniya(message, clas, number, cabinet, lesson):
         items.cabinet = cabinet
         items.class_letter = clas
         items.day = date
+
         db_sess.add(items)
         db_sess.commit()
         title = db_sess.query(Changes).filter(Changes.lesson == lesson, Changes.day == date).first()
         if title:
-            bot.send_message(message.from_user.id, 'Изменение было успешно сохраненно', reply_markup=start_keyboard())
+            bot.send_message(message.from_user.id, 'Изменение было успешно сохраненно',
+                             reply_markup=start_keyboard(message.from_user.id))
         else:
-            bot.send_message(message.from_user.id, 'Не удалось внести изменение, попробуйте ещё раз', reply_markup=start_keyboard())
-
+            bot.send_message(message.from_user.id, 'Не удалось внести изменение, попробуйте ещё раз',
+                             reply_markup=start_keyboard(message.from_user.id))
+        users = db_sess.query(User).filter(User.about == clas)
+        for user in users:
+            bot.send_message(user.user_id, f'Ваше расписание на {date} изменилось! Изменен {number} урок',
+                             reply_markup=start_keyboard(message.from_user.id))
+        bot.send_message(message.from_user.id, 'Ученики были уведомлены об изменениях',
+                         reply_markup=start_keyboard(message.from_user.id))
 
 
 def search_for(message):
